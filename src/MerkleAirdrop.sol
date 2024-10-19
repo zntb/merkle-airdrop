eac919544751e29971e2f50656aa0649e9d18417
zntb <zntbdev@proton.me>
2024-10-19 12:02:02 +0300

    Base Airdrop Contract

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,9 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract MerkleAirdrop {
+    using SafeERC20 for IERC20;
     // some list of addresses
     // Allow someone in the list to claim ERC-20 tokens
 
+    error MerkleAirdrop__InvalidProof();
+
     address[] claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airdropToken;
+
+    event Claim(address account, uint256 amount);
+
+    constructor(bytes32 merkleRoot, IERC20 airdropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airdropToken = airdropToken;
+    }
+
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // calculate using the account and the amount, the hash -> leaf node
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
 }
