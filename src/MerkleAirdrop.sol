a5dadd16372ac24aec039aa7d396e331d57868c6
zntb <zntbdev@proton.me>
2024-10-19 11:28:05 +0300

    Project Setup

---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract MerkleAirdrop {
+    // some list of addresses
+    // Allow someone in the list to claim ERC-20 tokens
+
+    address[] claimers;
+}
